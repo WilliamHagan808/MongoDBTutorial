b87331dea3605b66552df2ca3c1e2f8cf502cd09
William Hagan <williamhagan1728@yahoo.com>
2022-04-12 16:58:25 -0700

    add endpoints and validate via postman

--- routes/products.js
+++ routes/products.js
@@ -2,4 +2,15 @@ const Product = require("../models/product");
 const express = require("express");
 const router = express.Router();
 
+router.post("/" , async (req, res) => {
+    try {
+        let newProduct = await new Product(req.body);
+        await newProduct.save();
+        return res.status(201).send(newProduct);
+
+    } catch (error) {
+        return res.status(500).send('Internal Server Error: ${error}');
+    }
+})
+
 module.exports = router;
